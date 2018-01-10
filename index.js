9d8137ed14d224642b1b80ba41f08bd6351f07c6
Riyaad Azad <azad.riyaad@gmail.com>
2018-01-10 12:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${you}!"
-  return happyHolidaysTo(you)
+  return "Happy holidays, you!"
 }
 
 function happyHolidayTo(holiday, name) {
